430b9589dee0ea3f9fb1c1fde9e13c8d1d804def
Arathi Krishna  A M <130903958+arathikrishnaam@users.noreply.github.com>
2024-07-07 14:39:02 +0530

    Create hellosample.java

---
+++ hellosample.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class hellosample {
+    public static void main(String[] args) 
+        {   
+
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter 2 numbers");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("Sum is " + sum);
+            
+        }
+    
+}
+
+//java is platform independent 
